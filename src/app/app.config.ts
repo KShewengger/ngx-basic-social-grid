cc987b567ba99adf176f5fb102036964b2950e57
Kristy Mae Almuete <kristyalmuete@gmail.com>
2024-05-02 03:18:17 +0800

    #3 setup api services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { JSON_PLACEHOLDER_API } from '@app/core';
+import { environment } from '@environments/environment';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    { provide: JSON_PLACEHOLDER_API, useValue: environment.apiBaseUrl }
+  ]
 };
---
+++ src/app/core/tokens/api-token.ts
@@ -0,0 +1,3 @@
+import { InjectionToken } from '@angular/core';
+
+export const JSON_PLACEHOLDER_API = new InjectionToken('Json Placeholder API');
