258785b7ca6226dcd3393daa8c5bf737e649ef3a
Kristy Mae Almuete <kristyalmuete@gmail.com>
2024-05-05 02:01:52 +0800

    [DASHBOARD] fix mobile responsiveness #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { MatTooltip } from '@angular/material/tooltip';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NAVIGATION_ITEMS } from '@app/constants';
 import { UsersFacade } from '@states/users';
@@ -20,6 +21,7 @@ import { UsersFacade } from '@states/users';
     MatSidenav,
     MatSidenavContent,
     MatIcon,
+    MatTooltip,
   ],
 })
 export class AppComponent {
