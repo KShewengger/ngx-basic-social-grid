87aec5f7f121881515ddcd0a66e4c9f01e08cb35
Kristy Mae Almuete <kristyalmuete@gmail.com>
2024-05-06 12:08:30 +0800

    [REFACTOR] setup base filter abstract #28

---
+++ src/app/abstracts/base-filter.abstract.ts
@@ -0,0 +1,24 @@
+import { computed, effect, signal } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+
+export abstract class BaseFilter {
+  protected pageIndex = signal<number>(0);
+  protected pageEvent = signal<PageEvent | null>(null);
+
+  protected startPage = computed(
+    () => (this.pageEvent()?.pageIndex ?? 0) * (this.pageEvent()?.pageSize ?? 0)
+  );
+
+  protected endPage = computed(
+    () => this.startPage() + (this.pageEvent()?.pageSize ?? this.options.pageSize)
+  );
+
+  #handlePaginationUpdate = effect(
+    () => {
+      this.pageIndex.set(this.pageEvent()?.pageIndex ?? 0);
+    },
+    { allowSignalWrites: true }
+  );
+
+  constructor(public options: { pageSize: number }) {}
+}
--- src/app/features/albums/containers/albums/albums.component.ts
+++ src/app/features/albums/containers/albums/albums.component.ts
@@ -1,14 +1,8 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  effect,
-  inject,
-  signal
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { BaseFilter } from '@app/abstracts';
 import { AlbumUser } from '@app/models';
 import { filterDataBySearch } from '@app/utils';
 import { AlbumComponent } from '@features/common/album';
@@ -32,12 +26,10 @@ import { AlbumsFacade } from '@states/albums';
     MatProgressSpinner
   ]
 })
-export class AlbumsComponent {
+export class AlbumsComponent extends BaseFilter {
   private albumsFacade = inject(AlbumsFacade);
 
   public search = signal<string>('');
-  public pageIndex = signal<number>(0);
-  public pageEvent = signal<PageEvent | null>(null);
   public openedAlbum = signal<AlbumUser | null>(null);
 
   private albums = this.albumsFacade.usersAlbums;
@@ -51,20 +43,9 @@ export class AlbumsComponent {
 
   public totalFilteredAlbums = computed(() => this.filteredAlbums().length);
 
-  public startPage = computed(
-    () => (this.pageEvent()?.pageIndex ?? 0) * (this.pageEvent()?.pageSize ?? 0)
-  );
-
-  public endPage = computed(() => this.startPage() + (this.pageEvent()?.pageSize ?? this.pageSize));
-
-  private handlePaginationUpdate = effect(
-    () => {
-      this.pageIndex.set(this.pageEvent()?.pageIndex ?? 0);
-    },
-    { allowSignalWrites: true }
-  );
-
-  public readonly pageSize = 8;
+  constructor() {
+    super({ pageSize: 10 });
+  }
 
   public handleSearch(value: string) {
     this.pageIndex.set(0);
