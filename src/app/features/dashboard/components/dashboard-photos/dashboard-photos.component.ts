dc8c73dc891c89f43bd5e93bc61bced248d52aa0
Kristy Mae Almuete <kristyalmuete@gmail.com>
2024-05-06 13:24:15 +0800

    add loading on dashboard photos

--- src/app/features/dashboard/components/dashboard-photos/dashboard-photos.component.ts
+++ src/app/features/dashboard/components/dashboard-photos/dashboard-photos.component.ts
@@ -2,6 +2,7 @@ import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { PhotosFacade } from '@app/states/photos';
 import { PhotoComponent } from '@features/common/photo';
 
@@ -10,11 +11,21 @@ import { PhotoComponent } from '@features/common/photo';
   selector: 'sg-dashboard-photos',
   templateUrl: 'dashboard-photos.component.html',
   styleUrl: 'dashboard-photos.component.scss',
-  imports: [NgClass, MatCard, MatCardContent, MatIcon, MatCardHeader, MatCardTitle, PhotoComponent]
+  imports: [
+    NgClass,
+    MatCard,
+    MatCardContent,
+    MatIcon,
+    MatCardHeader,
+    MatCardTitle,
+    MatProgressSpinner,
+    PhotoComponent
+  ]
 })
 export class DashboardPhotosComponent {
   private photosFacade = inject(PhotosFacade);
 
   public photos = this.photosFacade.topPhotos;
   public totalPhotos = this.photosFacade.totalTopPhotos;
+  public loading = this.photosFacade.loading;
 }
