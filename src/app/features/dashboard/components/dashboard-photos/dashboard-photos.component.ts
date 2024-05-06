1298d0d972f2ff7d1124b648eed9a7b7ee2e93d7
Kristy Mae Almuete <kristyalmuete@gmail.com>
2024-05-06 11:34:18 +0800

    [REFACTOR] remove photo drawer support #28

--- src/app/features/dashboard/components/dashboard-photos/dashboard-photos.component.ts
+++ src/app/features/dashboard/components/dashboard-photos/dashboard-photos.component.ts
@@ -1,10 +1,8 @@
 import { NgClass } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
-import { Photo } from '@app/models';
 import { PhotosFacade } from '@app/states/photos';
-import { DrawerComponent } from '@features/common/drawer';
 import { PhotoComponent } from '@features/common/photo';
 
 @Component({
@@ -12,21 +10,10 @@ import { PhotoComponent } from '@features/common/photo';
   selector: 'sg-dashboard-photos',
   templateUrl: 'dashboard-photos.component.html',
   styleUrl: 'dashboard-photos.component.scss',
-  imports: [
-    NgClass,
-    MatCard,
-    MatCardContent,
-    MatIcon,
-    MatCardHeader,
-    MatCardTitle,
-    PhotoComponent,
-    DrawerComponent
-  ]
+  imports: [NgClass, MatCard, MatCardContent, MatIcon, MatCardHeader, MatCardTitle, PhotoComponent]
 })
 export class DashboardPhotosComponent {
   private photosFacade = inject(PhotosFacade);
-
-  public openedPhoto = signal<Photo | null>(null);
 
   public photos = this.photosFacade.topPhotos;
   public totalPhotos = this.photosFacade.totalTopPhotos;
