ca40500e285cd5cd235798bce6609209bec85d13
Kristy Mae Almuete <kristyalmuete@gmail.com>
2024-05-06 11:26:48 +0800

    [REFACTOR] setup dashboard photo drawer and set drawer to fixed position #28

--- src/app/features/dashboard/components/dashboard-photos/dashboard-photos.component.ts
+++ src/app/features/dashboard/components/dashboard-photos/dashboard-photos.component.ts
@@ -1,8 +1,10 @@
 import { NgClass } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
+import { Photo } from '@app/models';
 import { PhotosFacade } from '@app/states/photos';
+import { DrawerComponent } from '@features/common/drawer';
 import { PhotoComponent } from '@features/common/photo';
 
 @Component({
@@ -10,10 +12,21 @@ import { PhotoComponent } from '@features/common/photo';
   selector: 'sg-dashboard-photos',
   templateUrl: 'dashboard-photos.component.html',
   styleUrl: 'dashboard-photos.component.scss',
-  imports: [NgClass, MatCard, MatCardContent, MatIcon, MatCardHeader, MatCardTitle, PhotoComponent]
+  imports: [
+    NgClass,
+    MatCard,
+    MatCardContent,
+    MatIcon,
+    MatCardHeader,
+    MatCardTitle,
+    PhotoComponent,
+    DrawerComponent
+  ]
 })
 export class DashboardPhotosComponent {
   private photosFacade = inject(PhotosFacade);
+
+  public openedPhoto = signal<Photo | null>(null);
 
   public photos = this.photosFacade.topPhotos;
   public totalPhotos = this.photosFacade.totalTopPhotos;
